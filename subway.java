471de31310461db0f07a3c11c8c27d0b0cf9d509
Umabharathi886 <mumabharathi18@gmail.com>
2026-03-20 13:33:12 +0530

    task problems
    task problems

--- subway.java
+++ subway.java
@@ -1,2 +1,27 @@
+import java.util.*;
 public class subway{
+    public static void main(String[] args){
+     Scanner sc = new Scanner(System.in) ;
+     System.out.println("1 for jump");
+     System.out.println("2 for roll");
+     System.out.println("3 for moving left");
+     System.out.println("4 for moving right");
+     int n = sc.nextInt();
+     switch(n){
+        case 1:
+            System.out.print("player jumps over obstalce");
+            break;
+        case 2:
+            System.out.print("player rolls under the obstacle");
+            break;
+        case 3:
+            System.out.print("player moves right direction");
+            break;
+        case 4:
+            System.out.print("player move in left direction");
+            break;
+        default:
+            System.out.print("invalid move");
+     }
+    }
 }
